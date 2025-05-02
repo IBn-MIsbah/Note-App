f966ff5a82485997645263884147ac0ff44c6135
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-05-02 13:43:37 +0300

    add reference

--- models/note.js
+++ models/note.js
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const noteShema = new mongoose.Schema({
-    title: String,
-    content: String,
-    userID: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    createdAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now}
-})
-
-export default mongoose.model("Note", noteShema)
+  title: String,
+  content: String,
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  cratedAt: { type: Date, default: Date.now, imutable: true },
+  updatedAt: { type: Date, default: Date.now },
+});
+noteShema.pre("save", function (next) {
+  this.updatedAt = Date.now;
+});
+export default mongoose.model("Note", noteShema);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
-import { name, email, password } from 'ejs';
-import mongoose from 'mongoose';
+import { name, email, password } from "ejs";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    email: {type: String, require: true}, unique: true,
-    password: String,
-    cratedAt: Date.now
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true },
+  unique: true,
+  password: String,
+  cratedAt: { type: Date, default: Date.now, imutable: true },
+});
 
-export default mongoose.model("User", userSchema)
+export default mongoose.model("User", userSchema);
