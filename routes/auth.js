f25adf2bb4e43bfa42fcb96dd8e7e78dd6d6112b
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-05-08 21:30:12 +0300

    auht route for user

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,111 @@
-import express from 'express';
-import passport from 'passport';
-import User from '../models/user.js'
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("../models/user.js");
+const Note = require("../models/note.js")
+const { isLoggedIn } = require("../middleware/authMiddleware.js");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
-router.get('/login',(req, res)=>{
-    res.render('auth/login')
-})
-router.post('/register', async(req,res)=>{
-   const {name, email, password} = req.body
-   const newUser = new User({name, email})
+router.get("/login", (req, res) => {
+  res.render("auth/login", {
+    title: "LogIn",
+  });
+});
+router.get("/register", (req, res) => {
+  res.render("auth/register", {
+    title: "SignUp",
+  });
+});
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
 
-   User.register(newUser, password, (err, user)=>{
-    if(err){
-        console.error(err)
-        return res.render('auth/register')
+router.get('/auth/google/note-app', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  });
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res.redirect("/login");
     }
-    passport.authenticate('local')(req, res, ()=>{
-        res.redirect('/notes/dashboard');
-    })
-   })
-})
+    const newUser = new User({ username, email });
+    User.register(newUser, password, async (err, user) => {
+      if (err) {
+        console.error(err.message);
+        return res.redirect("/register");
+      }
+      passport.authenticate("local")(req, res, async () => {
+        const populatedUser = await User.findById(user._id).populate("notes");
+        res.render("notes/dashboard", {
+          title: "Dashboard",
+          user: populatedUser,
+          notes: populatedUser.notes,
+        });
+      });
+      
+    });
+  } catch (err) {
+    console.error(err);
+    console.error(err.message);
+    res.send(
+      "Someting went wrong. User could not register. Please try again!",
+      err.message
+    );
+  }
+});
 
-export default router;
+router.get("/dashboard", isLoggedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("notes");
+    if (!user) {
+      return res.redirect("/login");
+    }
+    console.log("User info:", user);
+
+    res.render("notes/dashboard", {
+      title: "Dashboard",
+      notes: user.notes,
+      user,
+    });
+  } catch (err) {
+    console.error("Dashboard error:", err);
+    res.redirect("/login");
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failWithError: true,
+  })
+);
+
+router.get("/logout", (req, res) => {
+  req.logOut((err) => {
+    if (err) {
+      res.status(500).send("error loging out");
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/notes/create", isLoggedIn, (req, res) => {
+  res.render("notes/create-note", {
+    user: req.user,
+    messages: {
+      error: req.flash("error"),
+      success: req.flash("success"),
+    },
+  });
+});
+
+module.exports = router;
